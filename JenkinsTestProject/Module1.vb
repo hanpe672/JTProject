3c90e409b6e7b65b3212579d9f10a473ac200838
hanpe672 <hannespersson80@gmail.com>
2016-10-10 11:40:34 +0200

    Adding files

---
+++ JenkinsTestProject/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+
+Public Module Module1
+
+    Sub Main()
+
+        Dim declaredINT1, declaredINT2 As Integer
+        declaredINT1 = 10
+        declaredINT2 = 20
+        declaredINT1 = declaredINT1 + declaredINT2
+        For index As Integer = 1 To 5
+            Console.WriteLine(index)
+        Next
+        declaredINT1 = Console.Read()
+    End Sub
+End Module
---
+++ JenkinsTestProject/TestingClass.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports NUnit.Framework
+
+<TestFixture()>
+Public Class TestingClass
+    <Test()>
+    Public Sub IsNull()
+        Dim nada As Object = Nothing
+
+        ' Classic syntax
+        Assert.IsNull(nada)
+
+        ' Helper syntax
+        Assert.That(nada, [Is].Null)
+    End Sub
+
+    <Test()>
+    Public Sub AdditionTest()
+        Dim a, b, c, d As Integer
+        a = 1
+        b = 2
+        d = 3
+        c = a + b
+        Assert.AreEqual(c, d)
+    End Sub
+
+End Class
